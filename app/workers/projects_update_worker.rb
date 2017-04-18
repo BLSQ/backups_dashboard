20ff96abbef70f69f72a8f1e427a78116dbddc5a
Nicolas Germeau <nicolas.germeau@gmail.com>
2017-04-18 16:13:05 +0200

    correcting uppercase bug

--- app/workers/projects_update_worker.rb
+++ app/workers/projects_update_worker.rb
@@ -4,12 +4,12 @@ class ProjectsUpdateWorker
   def perform()
     #project_service = ProjectService.new 
     #applications = project_service.retrieve_heroku_applications
-    #
+
    heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOOLBELT_API_PASSWORD'])
-   HEROKU_CMD = '/app/vendor/heroku-toolbelt/bin/heroku'
+   heroku_cmd = '/app/vendor/heroku-toolbelt/bin/heroku'
    heroku.app.list.each do |app|
      puts app['name']
-     puts `#{HEROKU_CMD} pg:backups --app #{app['name']}`
+     puts `#{heroku_cmd} pg:backups --app #{app['name']}`
    end
 
     puts "all applications #{applications}"
