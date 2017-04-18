0fcdf730af87225f027dbb4098d40662a64a887e
Nicolas Germeau <nicolas.germeau@gmail.com>
2017-04-18 16:04:19 +0200

    sidekiq calling heroku toolbelt

--- app/services/project_service.rb
+++ app/services/project_service.rb
@@ -2,4 +2,7 @@
 class ProjectService
   def initialize(params)
   end
+
+  def list_apps 
+  end 
 end 
--- app/workers/projects_update_worker.rb
+++ app/workers/projects_update_worker.rb
@@ -2,6 +2,16 @@ class ProjectsUpdateWorker
   include Sidekiq::Worker
 
   def perform()
-    puts "updating all projects is done"
+    #project_service = ProjectService.new 
+    #applications = project_service.retrieve_heroku_applications
+    #
+   heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOOLBELT_API_PASSWORD'])
+   HEROKU_CMD = '/app/vendor/heroku-toolbelt/bin/heroku'
+   heroku.app.list.each do |app|
+     puts app['name']
+     puts `#{HEROKU_CMD} pg:backups --app #{app['name']}`
+   end
+
+    puts "all applications #{applications}"
   end
 end
