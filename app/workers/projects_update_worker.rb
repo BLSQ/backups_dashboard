1d23e4aaeb92286d8850afa5dc4c1002e41fc9bd
Nicolas Germeau <nicolas.germeau@gmail.com>
2017-04-18 16:45:16 +0200

    correcting bug and verification that it list apps in the logs

--- app/workers/projects_update_worker.rb
+++ app/workers/projects_update_worker.rb
@@ -5,13 +5,12 @@ class ProjectsUpdateWorker
     #project_service = ProjectService.new 
     #applications = project_service.retrieve_heroku_applications
 
-   heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOOLBELT_API_PASSWORD'])
-   heroku_cmd = '/app/vendor/heroku-toolbelt/bin/heroku'
-   heroku.app.list.each do |app|
-     puts app['name']
-     puts `#{heroku_cmd} pg:backups --app #{app['name']}`
-   end
+     heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOOLBELT_API_PASSWORD'])
+     heroku_cmd = '/app/vendor/heroku-toolbelt/bin/heroku'
+     heroku.app.list.each do |app|
+       puts app['name']
+       puts `#{heroku_cmd} pg:backups --app #{app['name']}`
+     end
 
-    puts "all applications #{applications}"
   end
 end
