6d9530b47d59b92fa4d63d9b4b35c9ddeee9a521
Boris Rorsvort <boris.rorsvort@gmail.com>
2017-04-19 14:34:12 +0200

    Fix project list and add icons

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,12 @@
 module ProjectsHelper
+  def db_icon(db_connector)
+    path = case db_connector
+           when 'postgresql'
+             'postgresql.svg'
+           else
+             'mysql.svg'
+           end
+
+    image_tag path, width: 16, class: 'db-icon'
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,8 +2,8 @@ class Project < ApplicationRecord
   enum db_connector: %i[jaws_db clear_db postgresql]
   has_many :backups
 
-  scope :missing_config, -> { where.not(db_connector: :postgresql).where(autobus_token: nil) }
-  scope :configured, -> { where.not(autobus_token: nil) }
+  scope :missing_config, -> { where.not(db_connector: [Project.db_connectors[:postgresql]]).where(autobus_token: '') }
+  scope :configured, -> { where.not(autobus_token: '') }
 
   def configured?
     postgresql? ? true : autobus_token?
