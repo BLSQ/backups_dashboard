80c24cc1d17570c969c575bc2b9ef333650f9025
Nicolas Germeau <nicolas.germeau@gmail.com>
2017-04-20 16:43:19 +0200

    connecting worker to ProjectService

--- app/workers/projects_update_worker.rb
+++ app/workers/projects_update_worker.rb
@@ -2,15 +2,7 @@ class ProjectsUpdateWorker
   include Sidekiq::Worker
 
   def perform
-    #project_service = ProjectService.new 
-    #applications = project_service.retrieve_heroku_applications
-
-     heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOOLBELT_API_PASSWORD'])
-     heroku_cmd = '/app/vendor/heroku-toolbelt/bin/heroku'
-     heroku.app.list.each do |app|
-       puts app['name']
-       puts `#{heroku_cmd} pg:backups --app #{app['name']}`
-     end
-
+    service = ProjectService.new 
+    service.configure_apps
   end
 end
