792296eb4190558667d9bd2ff9e5114067f62fbf
Boris Rorsvort <boris.rorsvort@gmail.com>
2017-04-20 17:53:33 +0200

    Fix missing config scope

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   enum db_connector: %i[jaws_db clear_db postgresql]
   has_many :backups
 
-  scope :missing_config, -> { where.not(db_connector: [Project.db_connectors[:postgresql]]).where(autobus_token: '') }
+  scope :missing_config, -> { where.not(db_connector: [Project.db_connectors[:postgresql]]).where(autobus_token: ['', nil]) }
   scope :with_autobus_config, -> { where.not(autobus_token: '') }
 
   def self.configured
